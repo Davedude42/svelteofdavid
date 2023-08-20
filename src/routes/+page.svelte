d415684475551f5686f027c6dfa55fe157cf7e85
David Haroldsen <davidharoldsen42@gmail.com>
2023-08-19 21:22:52 -0500

    it's a great day to be a redwing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 </style>
 <div class="flex flex-col items-center justify-center p-4" style="padding-top: 120px;">
 	<div class="console-welcome flex gap-4" style="width: 100%; max-width: 800px;">
-		<img src={continentalDivideImage} alt="meee" class="thick-black-border comic-shadow block self-center" style="height: 400px;" />
+		<img src={meImage} alt="meee" class="thick-black-border comic-shadow block self-center" style="height: 400px;" />
 		<pre class="flex-grow thick-black-border comic-shadow p-6 text-white bg-neutral-800 font-IBMPlexMono whitespace-pre-wrap" bind:this={typewriterElement}>{@html typewriterText}</pre>
 	</div>
 </div>
@@ -25,17 +25,24 @@
 <script lang="ts">
 import { onMount, onDestroy } from 'svelte';
 import continentalDivideImage from '$lib/assets/imgs/continentaldivide.jpg';
+import greatDayImage from '$lib/assets/imgs/greatday.jpg';
 
-let text: string = `<span style="background-color: dodgerblue; color: black; font-weight: 600;"> Welcome! </span>  
+let whichText: number = Math.floor(Math.random() * 2);
+
+let text: string = `
+<span style="background-color: dodgerblue; color: black; font-weight: 600;"> Welcome! </span>  
       
 You may be wondering...  
  - What is this place?  
  - Why am I here?  
- - Who is this <span style="color: dodgerblue; font-weight: 600;">fine young man</span> standing next to the <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Continental_Divide_of_the_Americas" class="hover:underline">Continental Divide</a>? 
+ - Who is this <span style="color: dodgerblue; font-weight: 600;">fine young man</span> ${['standing next to the <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/Continental_Divide_of_the_Americas" class="hover:underline">Continental Divide</a>?', 'wearing a toga while embracing that it\'s a great day to be a redwing?'][whichText]}\u200b\u200b\u200b\u200b\u200b\u200b
 
 To answer all of these questions and more, navigate to the About section.  
   
-If you really don't care, skip straight to the Projects section.\u200b\u200b\u200b\u200b\u200b\u200b\u200b\u200b\u200b\u200b\u200b\u200b Up to you.  `;
+If you really don't care, skip straight to the Projects section.\u200b\u200b\u200b\u200b\u200b\u200b\u200b\u200b\u200b\u200b\u200b\u200b Up to you.  
+`.trim();
+
+let meImage: any = [continentalDivideImage, greatDayImage][whichText];
 
 let typewriterText: string = text;
 let typewriterIndex: number = 0;
