13741a356452bf38e8c74c4c43d25f960e8bc251
David Haroldsen <davidharoldsen42@gmail.com>
2023-08-19 19:47:26 -0500

    fixed livestream

--- src/routes/project/tuitionlivestream/+page.svelte
+++ src/routes/project/tuitionlivestream/+page.svelte
@@ -27,7 +27,7 @@ p, blockquote {
 	<h2>Tuition Live Stream</h2>
 	<p>This is calculated off the base tuition for 2023-2024.</p>
 	<p>If you have a scholarship or something, then good job!</p>
-	<p>It adds up during school days from 7:45am to 3pm.</p>
+	<p>The livestream runs during school days from 7:45am to 3pm.</p>
 </ProgramInfo>
 <div class="cash-money-background flex flex-col items-center gap-4 px-4" style="padding-top: 80px; padding-bottom: 24px;">
 	<div class="thick-black-border w-full p-4 bg-white" style="max-width: 600px">
@@ -41,25 +41,26 @@ p, blockquote {
 		<div class="text-2xl">~<span class="font-medium font-IBMPlexMono">${ Math.round(dayPrice) }</span> every day</div>
 	</div>
 	<div class="flex flex-col items-center thick-black-border w-full p-4 bg-white" style="max-width: 600px">
-		<div class="flex flex-row items-center justify-between w-full">
+		<div class="relative flex flex-row items-center justify-between w-full">
 			<div class="font-bold">Live Stream</div>
+			<i class="absolute top-0 right-0 text-xs fa-solid fa-circle" style:color={live ? 'red' : '#bbb'}></i>
 		</div>
 		<div class="flex flex-row items-center justify-between w-full py-4" style="max-width: 400px">
 			<div class="text-lg font-bold">This Year</div>
-			<div style="font-size: 42px; line-height: 1;" class="flex flex-row font-medium font-IBMPlexMono">
-				{#each { length: yearString.length - 2 } as _, index}
+			<div style="font-size: 42px; line-height: 1;" class="font-medium font-IBMPlexMono">
+				{#each yearString as _, index}
 					<CharacterChanger character={yearString.charAt(index)} />
 				{/each}
-				<span class="inline-block">{yearString.charAt(7)}</span>
-				<span class="inline-block">{yearString.charAt(8)}</span>
 			</div>
 		</div>
 		<div class="flex flex-row items-center justify-between w-full py-4" style="max-width: 400px">
 			<div class="text-lg font-bold">Today</div>
-			<div style="font-size: 42px; line-height: 1;" class="font-IBMPlexMono">
-				{#each todayString as _, index}
+			<div style="font-size: 42px; line-height: 1;" class="flex flex-row font-IBMPlexMono">
+				{#each { length: todayString.length - 2 } as _, index}
 					<CharacterChanger character={todayString.charAt(index)} />
 				{/each}
+				<span class="inline-block">{todayString.charAt(6)}</span>
+				<span class="inline-block">{todayString.charAt(7)}</span>
 			</div>
 		</div>
 	</div>
@@ -99,7 +100,7 @@ import CharacterChanger from "$lib/components/CharacterChanger.svelte";
 import ProgramInfo from '$lib/components/ProgramInfo.svelte';
 
 function isSchoolDay(d: Date): boolean {
-	if(d.getDay() === 0 || d.getDay() === 6) {
+	if(d.getDay() === 0 || d.getDay() === 6 || d < new Date(yearStartAndEnd[0]) || d > new Date(yearStartAndEnd[1])) {
 		return false;
 	}
 
@@ -119,6 +120,8 @@ function countSchoolDays(a: Date, b: Date): number {
 
 	tempDate.setHours(0, 0, 0, 0);
 	targetDate.setHours(0, 0, 0, 0);
+
+	if(targetDate < tempDate) return 0;
 
 	let sum: number = 0;
 
@@ -147,8 +150,6 @@ const excludedDays: string[][] = [
 
 let daysInYear: number = countSchoolDays(new Date(yearStartAndEnd[0]), new Date(yearStartAndEnd[1]));
 
-console.log(excludedDays, daysInYear);
-
 const dayPrice: number = yearPrice / daysInYear;
 
 const airpodPrice: number = 169;
@@ -163,6 +164,8 @@ let c = 0;
 let todayString: string = '';
 let yearString: string = '';
 
+let live: boolean = false;
+
 function updateLivestream(): void {
 	let now: Date = new Date();
 
@@ -173,19 +176,28 @@ function updateLivestream(): void {
 
 	let today: number = percentSchoolDay * dayPrice;
 	
-	if(now < new Date(yearStartAndEnd[0]) || now > new Date(yearStartAndEnd[1])) {
+	if(!isSchoolDay(now)) {
+		live = false
 		today = 0;
+	} else {
+		live = true;
 	}
 
 	let year: number = 0;
 
-	let schoolDays = countSchoolDays(new Date(yearStartAndEnd[0]), new Date());
+	let yesterday = new Date(now);
+	yesterday.setDate(yesterday.getDate() - 1);
 
-	year += Math.floor((schoolDays - 1) * dayPrice * 100) / 100;
+	let schoolDays = countSchoolDays(new Date(yearStartAndEnd[0]), yesterday);
+
+	year += Math.floor(schoolDays * dayPrice * 100) / 100; // rounded here so that "today" and "year" add cents at the same time
 	year += today;
 
-	todayString = ('$' + (Math.floor(today * 100) / 100).toFixed(2)).padStart(8, ' ');
-	yearString = ('$' + year.toFixed(4)).padStart(9, ' ');
+	year = Math.floor(year * 100) / 100;
+	today = Math.floor(today * 10000) / 10000;
+
+	yearString = ('$' + year.toFixed(2)).padStart(9, ' ');
+	todayString = ('$' + today.toFixed(4)).padStart(8, ' ');
 }
 
 updateLivestream();
