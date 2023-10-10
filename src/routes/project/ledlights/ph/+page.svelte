91cdacba1a7e3ae2f08cf76fba7ea1c4e96e8d2b
David Haroldsen <davidharoldsen42@gmail.com>
2023-10-09 21:25:25 -0500

    r a i n b o w lights

--- src/routes/project/ledlights/ph/+page.svelte
+++ src/routes/project/ledlights/ph/+page.svelte
@@ -1,15 +1,15 @@
-<div style="padding-top: 80px;" class="flex-grow flex flex-col items-center gap-4 p-8">
-	<article class="thick-black-border w-full p-4 font-semibold" style="max-width: 600px;">
+<div style="padding-top: 80px;" class="flex-grow flex flex-col items-center gap-8 p-8">
+	<article class="thick-black-border comic-shadow w-full p-4 bg-lighter font-semibold" style="max-width: 600px;">
 		<p class="pb-2 font-bold tracking-wider">Hey! Hey!</p>
 		<p>
-			Did you notice the <span style="color: red;">r</span><span style="color: orange;">a</span><span style="color: khaki;">i</span><span style="color: green;">n</span><span style="color: darkcyan;">b</span><span style="color: blue;">o</span><span style="color: purple;">w</span> lights behind me in my About photo??
+			Did you notice the <span class="font-IBMPlexMono text-white tracking-wider" style="text-shadow: #00000055 1px 1px;"><span style="color: red;">r</span><span style="color: orange;">a</span><span style="color: khaki;">i</span><span style="color: green;">n</span><span style="color: darkcyan;">b</span><span style="color: blue;">o</span><span style="color: purple;">w</span></span> lights behind me in my About photo??
 		</p>
 	</article>
-	<article class="thick-black-border flex flex-row items-center justify-between w-full p-4 font-bold" style="max-width: 600px;">
+	<article class="thick-black-border comic-shadow flex flex-row items-center justify-between w-full p-4 font-bold" style="max-width: 600px;">
 		<div>Here it is again if you missed it</div>
-		<img class="thick-black-border" src={mee} alt="me with my led lights yaaay">
+		<img src={mee} alt="me with my led lights yaaay">
 	</article>
-	<article class="thick-black-border w-full p-4 font-semibold" style="max-width: 600px;">
+	<article class="thick-black-border comic-shadow w-full p-4 font-semibold" style="max-width: 600px;">
 		<p class="pb-2">
 			Those are my LED lights. They're <a class="text-blue-500 hover:underline" href="https://www.adafruit.com/category/168">Adafruit Neopixels</a> connected to my Raspberry Pi, and they allow me to control each pixel individually. 
 		</p>
